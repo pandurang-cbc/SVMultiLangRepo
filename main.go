9397eca8299ac518f539666d6f10221caf75a8fb
pandurang-cbc <pparchande@cloudbees.com>
2026-02-26 12:22:57 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ type Payload struct {
 	Password string `json:"password"`
 }
 
-const password = "super_secret"
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	apiKey := "my-secret-key"
